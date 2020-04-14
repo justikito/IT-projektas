76110a63a2077f52e5efd4f4a3ae4901e66f04ad
justikito <justikito@users.noreply.github.com>
2020-04-14 17:49:04 +0300

    Viskas

---
+++ script.js
@@ -0,0 +1,25 @@
+function background1() {
+	document.body.style.backgroundImage  = "url('bg2.jpg')";
+}
+function background2() {
+	document.body.style.backgroundImage  = "url('bg.jpg')";
+}
+
+var dataReload=document.querySelectorAll("[data-reload]");
+var kalba = {
+	lt: {
+		works: "Darbai",
+		about: "Apie",
+		referrals: "Atsiliepimai",
+		contact: "Kontaktai",
+	}
+};
+
+	if(window.location.hash) {
+		if(window.location.hash === "#lt") {
+			works1.textContent=kalba.lt.works;
+			about1.textContent=kalba.lt.about;
+			referrals1.textContent=kalba.lt.referrals;
+			contact1.textContent=kalba.lt.contact;
+	}
+}
