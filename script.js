94f95770a89035fd6c4494926c6a28faf5073baa
justikito <justikito@users.noreply.github.com>
2020-04-14 18:01:40 +0300

    Kalbos

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ var kalba = {
 		works: "Darbai",
 		about: "Apie",
 		referrals: "Atsiliepimai",
-		contact: "Kontaktai",
+		contact: "Susisiek",
 	}
 };
 
 	if(window.location.hash) {
 		if(window.location.hash === "#lt") {
 			works1.textContent=kalba.lt.works;
+			works2.textContent=kalba.lt.works;
 			about1.textContent=kalba.lt.about;
 			referrals1.textContent=kalba.lt.referrals;
 			contact1.textContent=kalba.lt.contact;
